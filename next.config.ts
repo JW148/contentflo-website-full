ad8157fc40cfe5d21078eac50c44ec3ab5a444c4
jw148 <wiggin472@gmail.com>
2025-03-04 12:04:12 +0000

    Optimised component rendering using React Compiler

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default withPayload(withNextVideo(nextConfig));
